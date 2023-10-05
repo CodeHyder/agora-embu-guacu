f35093b1c2c3e421436cc38a60b70c88409ff6d7
micazev <micneaz@gmail.com>
2023-10-05 15:34:51 -0300

    teste commit

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,18 +1,24 @@
-import './about.css'
+import "./about.css";
 
 const About = () => {
-    return(
-        <section className="section flex">
-            <div className="about__container flex">
-                <h2 className='about__title'>O debate de Embu-Guaçu na palma de sua mão</h2>
-                <p className='about__text'>Faça denúncias, acompanhe, receba notificações sobre as principais questões do município.</p>
-            </div>
+  return (
+    <section className="section flex">
+      <div className="about__container flex">
+        <h2 className="about__title">
+          O debate de Embu-Guaçu na palma de sua mão
+        </h2>
+        <p className="about__text">
+          Faça denúncias, acompanhe, receba notificações sobre as principais
+          questões do município.
+        </p>
+        ooooi
+      </div>
 
-            <div className="about__img">
-                <div className="imagem"></div>
-            </div>
-        </section>
-    )
-}
+      <div className="about__img">
+        <div className="imagem"></div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
