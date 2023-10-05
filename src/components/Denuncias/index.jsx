9e01353d655ed7a2f666a19526717008fcb82985
micazev <micneaz@gmail.com>
2023-10-05 15:47:28 -0300

    classe servicos nos box de denuncia

--- src/components/Denuncias/index.jsx
+++ src/components/Denuncias/index.jsx
@@ -6,46 +6,49 @@ function Denuncias() {
     <div>
       <h1>Categoria da Denúncia</h1>
       <p>Selecione o tipo da sua denúncia.</p>
-      oioi
       <section className="categorias">
         <div className="services__container">
-          <img src="" alt="" />
-          <p>Saúde</p>
+          <img src="" alt="" className="services__icon" />
+          <p className="services__text">Saúde</p>
         </div>
         <div className="services__container">
           {" "}
-          <img src="/assets/icon_animals.png" alt="" />
-          <p>Segurança</p>
+          <img
+            src="/assets/icon_animals.png"
+            alt=""
+            className="services__icon"
+          />
+          <p className="services__text">Segurança</p>
         </div>
         <div className="services__container">
           {" "}
-          <img src="" alt="" />
-          <p>Moradia</p>
+          <img src="" alt="" className="services__icon" />
+          <p className="services__text">Moradia</p>
         </div>
         <div className="services__container">
           {" "}
-          <img src="" alt="" />
-          <p>Educação</p>
+          <img src="" alt="" className="services__icon" />
+          <p className="services__text">Educação</p>
         </div>
         <div className="services__container">
           {" "}
-          <img src="" alt="" />
-          <p>Estabelecimento</p>
+          <img src="" alt="" className="services__icon" />
+          <p className="services__text">Estabelecimento</p>
         </div>
         <div className="services__container">
           {" "}
-          <img src="" alt="" />
-          <p>Meio Ambiente</p>
+          <img src="" alt="" className="services__icon" />
+          <p className="services__text">Meio Ambiente</p>
         </div>
         <div className="services__container">
           {" "}
-          <img src="" alt="" />
-          <p>Mobilidade</p>
+          <img src="" alt="" className="services__icon" />
+          <p className="services__text">Mobilidade</p>
         </div>
         <div className="services__container">
           {" "}
-          <img src="" alt="" />
-          <p>Transparência</p>
+          <img src="" alt="" className="services__icon" />
+          <p className="services__text">Transparência</p>
         </div>
       </section>
       <div className="nav-denuncias">
